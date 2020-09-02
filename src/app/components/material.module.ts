2cff2d3ea6216f509a4b3bdbf97ed76a5d16c30d
Martin Bojnanský <martin.bojnansky@outlook.com>
2020-09-02 18:46:13 +0200

    Adds logo and popup help and feedback

--- src/app/components/material.module.ts
+++ src/app/components/material.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatCardModule],
+  exports: [CommonModule, MatButtonModule, MatIconModule, MatCardModule],
 })
 export class MaterialModule {}
